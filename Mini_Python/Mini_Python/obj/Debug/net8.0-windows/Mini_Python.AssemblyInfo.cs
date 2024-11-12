3a0b926d15a88c4f739a833293423129015dcbff
leiner alvarado <leineralvarador117@gmail.com>
2024-11-11 20:36:38 -0600

    add reportlist

--- Mini_Python/Mini_Python/obj/Debug/net8.0-windows/Mini_Python.AssemblyInfo.cs
+++ Mini_Python/Mini_Python/obj/Debug/net8.0-windows/Mini_Python.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini_Python")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d8c218626919ae67815f9218f0acda7cf5b1c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e407244b361622f47e2805b659ea7a99e70c3811")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini_Python")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini_Python")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
